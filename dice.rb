9467ab652ef7079c6df79bc20d4ebcbb7f96d8b9
Torell T. Pernell <torellpernell@yahoo.com>
2025-02-09 09:57:17 +0000

    First Routes added

---
+++ dice.rb
@@ -0,0 +1,16 @@
+# /dice.rb
+
+require "sinatra"
+require "sinatra/reloader"
+
+get("/") do
+  "Hello World"
+end
+
+get("/zebra") do
+  "We must add a route for each path we want to support"
+end
+
+get("/giraffe") do
+  "Hopefully this shows up without having to restart the server 🤞🏾"
+end
